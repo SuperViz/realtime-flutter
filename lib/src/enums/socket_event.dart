fb8a756af0f5e290a8b94a419bce858677e99559
Ismael Aquino <ismaellAquino@hotmail.com>
2024-09-26 08:17:38 -0300

    feat: increase amount of sockets events

--- lib/src/enums/socket_event.dart
+++ lib/src/enums/socket_event.dart
@@ -1,4 +1,12 @@
 enum SocketEvent {
+  connect('connect'),
+  disconnect('disconnect'),
+  connectError('connect_error'),
+  connectionError('error'),
+  reconnect('reconnect'),
+  reconnectAttempt('reconnect_attempt'),
+  reconnectError('reconnect_error'),
+  reconnectFailed('reconnect_failed'),
   error('socket-event.error');
 
   final String description;
