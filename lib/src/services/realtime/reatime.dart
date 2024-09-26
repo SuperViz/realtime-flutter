990e23ad2b70f1b3596e230cd85fe01801486318
Ismael Aquino <ismaellAquino@hotmail.com>
2024-09-25 22:44:36 -0300

    feat: add socker and client connection dependencies on realtime

--- lib/src/services/realtime/reatime.dart
+++ lib/src/services/realtime/reatime.dart
@@ -1,1 +1,12 @@
-class Realtime {}
+import '../../interfaces/interfaces.dart';
+import '../connections/connections.dart';
+
+class Realtime {
+  final SocketClient socket;
+  final ClientConnection clientConnection;
+
+  Realtime({
+    required this.socket,
+    required this.clientConnection,
+  });
+}
