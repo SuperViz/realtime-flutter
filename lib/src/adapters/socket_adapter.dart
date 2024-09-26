b291788a10739a3bcd786ad44edd15bd2154f24b
Ismael Aquino <ismaellAquino@hotmail.com>
2024-09-26 19:41:47 -0300

    feat: parse params on socket connect method

--- lib/src/adapters/socket_adapter.dart
+++ lib/src/adapters/socket_adapter.dart
@@ -1,23 +1,19 @@
 import 'dart:async';
 
-import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'package:socket_io_client/socket_io_client.dart' as io;
 
 import '../interfaces/interfaces.dart';
+import '../interfaces/params/params.dart';
 
 final class IoSocketAdapter implements SocketClient {
   IoSocketAdapter();
 
-  IO.Socket? socket;
+  io.Socket? socket;
 
   @override
   FutureOr<void> disconnect() async {
     if (socket == null) return;
-
-    print('disconnecting...');
-
     socket!.close();
-
-    print('Desconected');
   }
 
   @override
@@ -28,34 +24,30 @@ final class IoSocketAdapter implements SocketClient {
   }
   
   @override
-  FutureOr<IoSocketAdapter> connect() async {
+  FutureOr<IoSocketAdapter> connect(SocketConnectParams connectParams) async {
     if (socket != null) return IoSocketAdapter();
 
     print("Connecting...");
 
-    socket = IO.io(
-      'https://io.superviz.com',
-      IO.OptionBuilder()
+    socket = io.io(
+      'https://io.superviz.com/${connectParams.environment}',
+      io.OptionBuilder()
+        .setTransports(['websocket'])
         .enableReconnection()
         .setReconnectionDelay(1000)
         .setReconnectionDelayMax(5000)
         .setReconnectionAttempts(5)
         .setExtraHeaders({
-          'sv-api-key': 'neydzn8hlkhn2bymaq3rcabaxa5v3z',
+          'sv-api-key': connectParams.apiKey,
         })
         .setAuth({
-          'apiKey': 'neydzn8hlkhn2bymaq3rcabaxa5v3z',
-          'envirioment': 'dev',
-          'secret': 'sk_7y7aq70twj7nd2vy7ss4mbxomhb9w1',
-          'clientId': '499a215e-c957-46b3-a81c-f0a8de19defa',
+          'apiKey': connectParams.apiKey,
+          'envirioment': connectParams.environment,
+          'secret': connectParams.secretKey,
+          'clientId': connectParams.clientId,
         })
         .build()
     );
-
-    socket!.onConnect((ev) {
-      print("Connected");
-      print(ev);
-    });
 
     return IoSocketAdapter();
   }
--- lib/src/interfaces/socket_client.dart
+++ lib/src/interfaces/socket_client.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 
+import 'params/params.dart';
+
 abstract interface class SocketClient {
-  FutureOr<SocketClient> connect();
+  FutureOr<SocketClient> connect(SocketConnectParams connectionParams);
   FutureOr<void> disconnect();
   void emit(String event, [dynamic data]);
   void onEvent(String event, dynamic Function(dynamic) handlerCallback);
