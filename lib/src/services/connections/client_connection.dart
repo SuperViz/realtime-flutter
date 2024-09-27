eae1d4365403728fd3a0dca528f085719158f06b
Ismael Aquino <ismaellAquino@hotmail.com>
2024-09-27 13:44:20 -0300

    fix: errors on handle exceptions

--- lib/src/services/connections/client_connection.dart
+++ lib/src/services/connections/client_connection.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import '../../enums/enums.dart';
-import '../../exceptions/exceptions.dart';
 import '../../interfaces/interfaces.dart';
 import '../../types/types.dart';
 import '../../utils/utils.dart';
@@ -10,14 +9,14 @@ import 'connection_state.dart';
 final class ClientConnection {
   final SocketClient _socket;
 
-  late Logger logger;
+  late Logger _logger;
   late StreamController<ConnectionState> _stateController;
-  late ClientState state;
+  ClientState state = ClientState.disconnected;
 
   ClientConnection({
     required SocketClient socket,
   }) : _socket = socket {
-    logger = DebuggerLoggerAdapter(scope: '[SuperViz]');
+    _logger = DebuggerLoggerAdapter(scope: '@superviz/socket-client/realtime');
     _subscribeToManagerEvents();
     _stateController = StreamController<ConnectionState>();
   }
@@ -40,67 +39,67 @@ final class ClientConnection {
   }
 
   void _subscribeToManagerEvents() {
-    _socket.onEvent(SocketEvent.connect.description, (_) => _onConnect);
-    _socket.onEvent(SocketEvent.reconnect.description, (_) => _onReconnect);
-    _socket.onEvent(SocketEvent.disconnect.description, (_) => _onDisconnect);
-    _socket.onEvent(SocketEvent.connectError.description, _onConnectError);
-    _socket.onEvent(SocketEvent.reconnectError.description, _onReconnectError);
-    _socket.onEvent(SocketEvent.connectionError.description, _onConnectionError);
-    _socket.onEvent(SocketEvent.reconnectFailed.description, (_) => _onReconnectFailed);
-    _socket.onEvent(SocketEvent.reconnectAttempt.description, _onReconnecAttempt);
+    _socket.onEvent(SocketEvent.connect.description, (_) => _onConnect());
+    _socket.onEvent(SocketEvent.reconnect.description, (_) => _onReconnect());
+    _socket.onEvent(SocketEvent.disconnect.description, (_) => _onDisconnect());
+    _socket.onEvent(SocketEvent.reconnectFailed.description, (_) => _onReconnectFailed());
+    _socket.onEvent(SocketEvent.connectError.description, (error) => _onConnectError(error));
+    _socket.onEvent(SocketEvent.reconnectError.description, (error) => _onReconnectError(error));
+    _socket.onEvent(SocketEvent.connectionError.description, (error) => _onConnectionError(error));
+    _socket.onEvent(SocketEvent.reconnectAttempt.description, (attempt) => _onReconnecAttempt(attempt));
   }
 
    /// Change the state of the connection
-  void _changeConnectionState(ClientState state, [String? reason]) {
-    this.state = state;
+  void _changeConnectionState(ClientState newState, [String? reason]) {
+    state = newState;
 
     if (_stateController.isClosed) return;
 
     _stateController.add(
       ConnectionState(
-        state: state,
+        state: newState,
         reason: reason,
       ),
     );
   }
 
   void _onConnect() {
-    logger.log(name: 'connection @ on connect', description: 'Connected to the socket');
+    _logger.log(name: 'connection @ on connect', description: 'Connected to the socket');
     _changeConnectionState(ClientState.connected);
   }
 
   void _onDisconnect() {
-    logger.log(name: 'connection @ on disconnect', description: 'Disconnected from the socket');
+    _logger.log(name: 'connection @ on disconnect', description: 'Disconnected from the socket');
     _changeConnectionState(ClientState.disconnected);
   }
 
   void _onReconnect() {
-    logger.log(name: 'connection @ on reconnect', description: 'Reconnected to the socket');
+    _logger.log(name: 'connection @ on reconnect', description: 'Reconnected to the socket');
     _changeConnectionState(ClientState.connected);
   }
 
   void _onReconnectFailed() {
-    logger.log(name: 'connection @ on reconnect failed', description: 'Failed to reconnect to the socket');
+    _logger.log(name: 'connection @ on reconnect failed', description: 'Failed to reconnect to the socket');
     _changeConnectionState(ClientState.reconnectError);
   }
 
-  void _onConnectError(SocketException error) {
-    logger.log(name: 'connection @ on connect error', description: 'Connection error', error: error);
+  void _onConnectError(String error) {
+    _logger.log(name: 'connection @ on connect error', description: 'Connection error', error: Exception(error));
     _changeConnectionState(ClientState.connectionError);
   }
 
-  void _onConnectionError(SocketException error) {
-    logger.log(name: 'connection @ on connection error', description: 'Connection error', error: error);
-    _changeConnectionState(ClientState.connectionError, error.reason);
+  void _onConnectionError(String error) {
+    _logger.log(name: 'connection @ on connection error', description: 'Connection error', error: Exception(error));
+    _changeConnectionState(ClientState.connectionError, error);
   }
 
-  void _onReconnectError(SocketException error) {
-    logger.log(name: 'connection @ on reconnect error', description: 'Reconnect error');
-    _changeConnectionState(ClientState.reconnectError, error.reason);
+  void _onReconnectError(String error) {
+    _logger.log(name: 'connection @ on reconnect error', description: 'Reconnect error');
+    _changeConnectionState(ClientState.reconnectError, error);
   }
 
   void _onReconnecAttempt(int attempt) {
-    logger.log(name: 'connection @ on reconnect attempt', description: 'Reconnect attempt $attempt');
+    _logger.log(name: 'connection @ on reconnect attempt', description: 'Reconnect attempt $attempt');
     _changeConnectionState(ClientState.reconnecting, 'Reconnect attempt $attempt');
   }
 }
