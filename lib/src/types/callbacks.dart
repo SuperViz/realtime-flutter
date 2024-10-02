605711e64b49d3a5d589bee3fc2d700a610f4f1d
Ismael Aquino <ismaellAquino@hotmail.com>
2024-10-02 08:53:23 -0300

    feat: add generics on event callback

--- lib/src/types/callbacks.dart
+++ lib/src/types/callbacks.dart
@@ -2,6 +2,6 @@ typedef ErrorCallback = void Function(Object error, StackTrace stackTrace);
 
 typedef EventHandler = dynamic Function(dynamic);
 
-typedef EventCallback = void Function(dynamic);
+typedef EventCallback<T extends dynamic> = void Function(T data);
 
 typedef ValueChanged<P> = void Function(P value);
