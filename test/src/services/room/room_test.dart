459fb4e4a7e52519c70c57e3b0a8a8f64007766a
Ismael Aquino <ismaellAquino@hotmail.com>
2024-10-03 16:32:53 -0300

    feat: create room test

---
+++ test/src/services/room/room_test.dart
@@ -0,0 +1,15 @@
+import 'package:mockito/annotations.dart';
+import 'package:test/test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:faker/faker.dart';
+
+import 'package:realtime/src/enums/enums.dart';
+import 'package:realtime/src/interfaces/interfaces.dart';
+import 'package:realtime/src/services/room/models/models.dart';
+import 'package:realtime/src/services/room/room.dart';
+
+import 'room_test.mocks.dart';
+
+@GenerateMocks([SocketClient])
+void main() {
+}
