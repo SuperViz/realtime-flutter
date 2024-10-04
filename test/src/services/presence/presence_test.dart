f48241c79e334b262fd818c487f77021afcc09ba
Ismael Aquino <ismaellAquino@hotmail.com>
2024-10-04 15:31:56 -0300

    feat: create test to get method

--- test/src/services/presence/presence_test.dart
+++ test/src/services/presence/presence_test.dart
@@ -12,4 +12,43 @@ import 'presence_test.mocks.dart';
 
 @GenerateMocks([SocketClient])
 void main() {
+  late MockSocketClient mockSocketClient;
+  late String roomId;
+  late UserPresence user;
+
+  late PresenceRoom presence;
+
+  setUpAll(() {
+    mockSocketClient = MockSocketClient();
+    roomId = faker.conference.name();
+    user = UserPresence(
+      id: faker.guid.guid(),
+      name: faker.person.name(),
+    );
+  });
+
+  setUp(() {
+    presence = PresenceRoom.register(
+      mockSocketClient,
+      user,
+      roomId,
+    );
+  });
+
+  group('get method', () {
+    late final String event;
+
+    setUpAll(() {
+      event = InternalPresenceEvents.get.description;
+    });
+
+    test('Should socket register and emit presence.get', () {
+      presence.get((data) {});
+
+      verifyInOrder([
+        mockSocketClient.onEvent(event, any),
+        mockSocketClient.emit(event, roomId)
+      ]);
+    });
+  });
 }
