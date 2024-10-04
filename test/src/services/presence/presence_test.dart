7dcb61dc1be4e55a4e3aad167441bb3251f6a644
Ismael Aquino <ismaellAquino@hotmail.com>
2024-10-04 15:31:02 -0300

    feat: create initial tests files to presence

---
+++ test/src/services/presence/presence_test.dart
@@ -0,0 +1,15 @@
+import 'package:faker/faker.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+import 'package:realtime/src/enums/enums.dart';
+import 'package:realtime/src/interfaces/interfaces.dart';
+import 'package:realtime/src/services/presence/presence.dart';
+import 'package:realtime/src/services/room/models/models.dart';
+
+import 'presence_test.mocks.dart';
+
+@GenerateMocks([SocketClient])
+void main() {
+}
