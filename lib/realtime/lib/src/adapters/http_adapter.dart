865ed1371000f09c0ef78a11960f7f4615462d4d
Ismael Aquino <ismaellAquino@hotmail.com>
2024-10-17 14:32:23 -0300

    fix: change implementation of http client

--- lib/realtime/lib/src/adapters/http_adapter.dart
+++ lib/realtime/lib/src/adapters/http_adapter.dart
@@ -3,8 +3,9 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 import '../exceptions/exceptions.dart';
-import '../interfaces/http/http_client.dart';
-import '../interfaces/http/http_response.dart';
+import '../interfaces/interfaces.dart';
+
+export '../interfaces/interfaces.dart' show HttpMethod;
 
 final class HttpAdapter implements HttpClient {
   final http.Client httpClient;
@@ -12,21 +13,39 @@ final class HttpAdapter implements HttpClient {
   const HttpAdapter({required this.httpClient});
 
   @override
-  Future<HttpResponse> delete({
+  Future<HttpResponse> request({
     required String url,
+    required HttpMethod method,
     Map<String, String>? headers,
     Object? body,
   }) async {
     try {
       final uri = Uri.parse(url);
 
-      final response = await httpClient.delete(
-        uri,
-        headers: headers,
-        body: body,
-      );
+      late final http.Response response;
 
-      return _formatResponseJson(response.body);
+      switch (method) {
+        case HttpMethod.post:
+          response = await httpClient.post(uri, headers: headers, body: body);
+        case HttpMethod.put:
+          response = await httpClient.put(uri, headers: headers, body: body);
+        case HttpMethod.get:
+          response = await httpClient.get(uri, headers: headers);
+        case HttpMethod.delete:
+          response = await httpClient.delete(uri, headers: headers, body: body);
+      }
+
+      switch (response.statusCode) {
+        case 401:
+          throw HttpException(
+            message: response.body,
+            status: response.statusCode,
+          );
+        default:
+          return _formatResponseJson(response.body);
+      }
+    } on HttpException {
+      rethrow;
     } catch (e) {
       throw HttpException(
         message: 'Unexpected error.',
@@ -35,75 +54,7 @@ final class HttpAdapter implements HttpClient {
     }
   }
 
-  @override
-  Future<HttpResponse> get({
-    required String url,
-    Map<String, String>? headers,
-  }) async {
-    try {
-      final uri = Uri.parse(url);
-
-      final response = await httpClient.get(
-        uri,
-        headers: headers,
-      );
-
-      return _formatResponseJson(response.body);
-    } catch (e) {
-      throw HttpException(
-        message: 'Unexpected error.',
-        status: null,
-      );
-    }
-  }
-
-  @override
-  Future<HttpResponse> post({
-    required String url,
-    Map<String, String>? headers,
-    Object? body,
-  }) async {
-    try {
-      final uri = Uri.parse(url);
-
-      final response = await httpClient.post(
-        uri,
-        body: body,
-        headers: headers,
-      );
-
-      return _formatResponseJson(response.body);
-    } catch (e) {
-      throw HttpException(
-        message: 'Unexpected error.',
-        status: null,
-      );
-    }
-  }
-
-  @override
-  Future<HttpResponse> put({
-    required String url,
-    Map<String, String>? headers,
-    Object? body,
-  }) async {
-    try {
-      final uri = Uri.parse(url);
-
-      final response = await httpClient.put(
-        uri,
-        body: body,
-        headers: headers,
-      );
-
-      return _formatResponseJson(response.body);
-    } catch (e) {
-      throw HttpException(
-        message: 'Unexpected error.',
-        status: null,
-      );
-    }
-  }
+  
 
   HttpResponse _formatResponseJson(String json) {
     if (json.isEmpty) return HttpResponse(data: null);
--- lib/realtime/lib/src/interfaces/http/http_client.dart
+++ lib/realtime/lib/src/interfaces/http/http_client.dart
@@ -1,22 +1,9 @@
 import 'http.dart';
 
 abstract interface class HttpClient {
-  Future<HttpResponse> delete({
+  Future<HttpResponse> request({
     required String url,
-    Map<String, String>? headers,
-    Object? body,
-  });
-  Future<HttpResponse> get({
-    required String url,
-    Map<String, String>? headers,
-  });
-  Future<HttpResponse> post({
-    required String url,
-    Map<String, String>? headers,
-    Object? body,
-  });
-  Future<HttpResponse> put({
-    required String url,
+    required HttpMethod method,
     Map<String, String>? headers,
     Object? body,
   });
